e21eadcd88f8d09b017dc00be9110bea11bbc17a
mhsn555 <amohsen9246@gmail.com>
2026-03-10 17:50:41 +0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,39 @@
+
+public class Employee extends Person {
+	private String department;
+	private double baseSalary;
+	
+	public Employee(String id, String name, int age, String department, double baseSalary) {
+		super(id,name,age);
+		this.department = department;
+		this.baseSalary = baseSalary;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+	
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+	
+	public String toString() {
+		return "name: " + getName() + "  ID: " + getId() + "  age: " + getAge() + "  Department: " + department + "  Base Salary: " + baseSalary;
+	}
+	
+	
+	public String getRoleInfo() {
+		return"";
+	} // needs modification
+	
+	
+
+}
