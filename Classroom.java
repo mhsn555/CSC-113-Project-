5a65aa624840743492b5efba7245bee81ec85842
mhsn555 <amohsen9246@gmail.com>
2026-03-10 16:52:07 +0300

    Add files via upload

---
+++ Classroom.java
@@ -0,0 +1,31 @@
+
+public class Classroom {
+	private String roomNumber;
+	private String className;
+	private int capacity;
+	
+	public Classroom(String roomNumber,String className,int capacity) {
+		this.roomNumber = roomNumber;
+		this.className = className;
+		this.capacity = capacity;
+	}
+	
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+	
+	public String getClassName() {
+		return className;
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	public String toString() {
+		return "Class's name: " + className + "  number: " + roomNumber + "  Capacity: " + capacity;
+	}
+	
+	Classroom [] classrooms;
+
+}
