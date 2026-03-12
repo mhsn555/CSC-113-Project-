6f0219cc3421ac2b0ea6a57a99dc5e85257c1747
Alfarouq Alshukri <alfarouq.developer@gmail.com>
2026-03-12 17:19:34 +0300

    modified AdminStaff Class

--- AdminStaff.java
+++ AdminStaff.java
@@ -1,29 +1,22 @@
 
 public class AdminStaff extends Employee {
 	private String position;
-	private double allowance;
 	
-	public AdminStaff(String id, String name, int age, String department, double baseSalary,String position ,double allowance) {
-		super(id,name,age,department,baseSalary);
+	public AdminStaff(String id, String name, int age, String department, double baseSalary,double allowance, String position) {
+		super(id,name,age,department,baseSalary, allowance);
 		this.position = position;
-		this.allowance = allowance;
 	}
 
 	public String getPosition() {
 		return position;
 	}
 	
-	public double getAllowance() {
-		return allowance;
-	}
-	
-	
 	public String getRoleInfo() {
     return "Admin Staff - Position: " + position + ", Department: " + getDepartment();
 	}
 	
 	public String toString() {
-		return "name: " + getName() + "  ID: " + getId() + "  age: " + getAge() + "  Department: " + getDepartment() + "  Base Salary: " + getBaseSalary() + "  Position: " + position + "  Allowance: " + allowance;
+		return super.toString() + "  Position: " + position;
 	} 
 	
 	
