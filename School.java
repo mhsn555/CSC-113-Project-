c2c8ee5b021457336136a696d9fd3c5679344696
mhsn555 <amohsen9246@gmail.com>
2026-05-09 01:09:59 +0300

    Update School.java
    changed array to linkedlist

--- School.java
+++ School.java
@@ -1,208 +1,206 @@
 import java.io.*;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 
 public class School implements Payable {
 	private String name;
-	private int peopleCount;
-	private int classroomCount;
-	private int clubCount;
-
-	private Person[] people;
-	private Classroom[] classrooms;
-	private Club[] clubs;
+	private LinkedList<Person> people;
+	private LinkedList<Classroom> classrooms;
+	private LinkedList<Club> clubs;
 	
 	
 
-	public School(String name, int peopleSize, int classroomSize, int clubSize) {
+	public School(String name, int peopleSize, int classroomSize) {
 		this.name = name;
-		this.people = new Person[peopleSize];
-		this.classrooms = new Classroom[classroomSize];
-		this.clubs = new Club[clubSize];
-		this.peopleCount = 0;
-		this.classroomCount = 0;
-		this.clubCount = 0;
+		this.people = new LinkedList<>();
+		this.classrooms = new LinkedList<>();
+		this.clubs = new LinkedList<>();
 	}
 	
 	
 
 	public boolean addPerson(Person p) {
-		if (peopleCount >= people.length) {
-			System.out.println("Cannot add more people.");
-			return false;
-		}
 
-		if (searchPersonById(p.getId()) != null) {
-			System.out.println("There is already a person with this ID.");
-			return false;
-		}
+	    if (searchPersonById(p.getId()) != null) {
+	        System.out.println("There is already a person with this ID.");
+	        return false;
+	    }
 
-		people[peopleCount++] = p;
-		return true;
+	    people.add(p);
+	    return true;
 	}
-
+	
 	public boolean removePersonById(String id) {
-		for (int i = 0; i < peopleCount; i++) {
-			if (id.equals(people[i].getId())) {
-				if (people[i] instanceof Student) {
-					for (int j = 0; j < clubCount; j++) {
-						clubs[j].removeMemberById(id);
-					}
-				}
-				
-				if (people[i] instanceof Teacher) {
-					for (int j = 0; j < clubCount; j++) {
-						Teacher supervisor = clubs[j].getSupervisor();
-						if (supervisor != null && id.equals(supervisor.getId())) {
-							clubs[j].setSupervisor(null);
-						}
-					}
-				}
 
-				for (int j = i; j < peopleCount - 1; j++) {
-					people[j] = people[j + 1];
-				}
+	    for (Person p : people) {
 
-				people[--peopleCount] = null;
-				return true;
-			}
-		}
+	        if (id.equals(p.getId())) {
 
-		System.out.println("There is no person with that ID.");
-		return false;
+	            if (p instanceof Student) {
+
+	                for (Club c : clubs) {
+	                    c.removeMemberById(id);
+	                }
+	            }
+
+	            if (p instanceof Teacher) {
+
+	                for (Club c : clubs) {
+
+	                    Teacher supervisor = c.getSupervisor();
+
+	                    if (supervisor != null &&
+	                        id.equals(supervisor.getId())) {
+
+	                        c.setSupervisor(null);
+	                    }
+	                }
+	            }
+
+	            people.remove(p);
+	            return true;
+	        }
+	    }
+
+	    System.out.println("There is no person with that ID.");
+	    return false;
 	}
 
 	public Person searchPersonById(String id) {
-		for (int i = 0; i < peopleCount; i++) {
-			if (id.equals(people[i].getId())) {
-				return people[i];
+		for (int i = 0 ; i < people.size() ; i++) {
+			if (id.equals(people.get(i).getId())) {
+				return people.get(i);
 			}
 		}
 		return null;
 	}
 
 	public boolean addClassroom(Classroom c) {
-		if (classroomCount >= classrooms.length) {
-			System.out.println("Cannot add more classrooms.");
-			return false;
-		}
 
-		if (searchClassroomByRoomNumber(c.getRoomNumber()) != null) {
-			System.out.println("There is already a classroom with this room number.");
-			return false;
-		}
+	    if (searchClassroomByRoomNumber(c.getRoomNumber()) != null) {
+	        System.out.println("There is already a classroom with this room number.");
+	        return false;
+	    }
 
-		classrooms[classroomCount++] = c;
-		return true;
+	    classrooms.add(c);
+	    return true;
 	}
 
 	public Classroom searchClassroomByRoomNumber(String roomNumber) {
-		for (int i = 0; i < classroomCount; i++) {
-			if (roomNumber.equals(classrooms[i].getRoomNumber())) {
-				return classrooms[i];
-			}
+		for (Classroom c : classrooms) {
+
+		    if (roomNumber.equals(c.getRoomNumber())) {
+		        return c;
+		    }
 		}
 		return null;
 	}
 
 	public boolean addClub(Club c) {
-		if (clubCount >= clubs.length) {
-			System.out.println("Cannot add more clubs.");
-			return false;
-		}
 
-		if (searchClubByName(c.getClubName()) != null) {
-			System.out.println("There is already a club with this name.");
-			return false;
-		}
+	    if (searchClubByName(c.getClubName()) != null) {
+	        System.out.println("There is already a club with this name.");
+	        return false;
+	    }
 
-		clubs[clubCount++] = c;
-		return true;
+	    clubs.add(c);
+	    return true;
 	}
 
 	public boolean removeClubByName(String clubName) {
-		for (int i = 0; i < clubCount; i++) {
-			if (clubName.equals(clubs[i].getClubName())) {
-				for (int j = 0; j < peopleCount; j++) {
-					if (people[j] instanceof Student) {
-						((Student) people[j]).leaveClubSilently(clubName);
-					}
-				}
-				
-				for (int j = i; j < clubCount - 1; j++) {
-					clubs[j] = clubs[j + 1];
-				}
 
-				clubs[--clubCount] = null;
-				return true;
-			}
-		}
+	    for (Club c : clubs) {
 
-		System.out.println("There is no club with that name.");
-		return false;
+	        if (clubName.equals(c.getClubName())) {
+
+	            for (Person p : people) {
+
+	                if (p instanceof Student) {
+	                    ((Student) p).leaveClubSilently(clubName);
+	                }
+	            }
+
+	            clubs.remove(c);
+	            return true;
+	        }
+	    }
+
+	    System.out.println("There is no club with that name.");
+	    return false;
 	}
 
 	public Club searchClubByName(String clubName) {
-		for (int i = 0; i < clubCount; i++) {
-			if (clubName.equals(clubs[i].getClubName())) {
-				return clubs[i];
-			}
-		}
-		return null;
+
+	    for (Club c : clubs) {
+
+	        if (clubName.equals(c.getClubName())) {
+	            return c;
+	        }
+	    }
+
+	    return null;
 	}
 	
 	public Club searchClubBySupervisorId(String supervisorId) {
-		for (int i = 0; i < clubCount; i++) {
-			Teacher supervisor = clubs[i].getSupervisor();
-			if (supervisor != null && supervisorId.equals(supervisor.getId())) {
-				return clubs[i];
-			}
-		}
-		return null;
-	}
 
-	public void displayAllPeople() {
-		if (peopleCount == 0) {
-			System.out.println("There are no people in the school.");
-			return;
-		}
+	    for (Club c : clubs) {
 
-		for (int i = 0; i < peopleCount; i++) {
-			System.out.println(people[i].toString());
-			System.out.println("Role Info: " + people[i].getRoleInfo());
-			System.out.println("**************************");
-		}
+	        Teacher supervisor = c.getSupervisor();
+
+	        if (supervisor != null &&
+	            supervisorId.equals(supervisor.getId())) {
+
+	            return c;
+	        }
+	    }
+
+	    return null;
 	}
 
 	public void displayAllClassrooms() {
-		if (classroomCount == 0) {
-			System.out.println("There are no classrooms in the school.");
-			return;
-		}
 
-		for (int i = 0; i < classroomCount; i++) {
-			System.out.println(classrooms[i].toString() + "\n **************************");
-		}
+	    if (classrooms.isEmpty()) {
+	        System.out.println("There are no classrooms in the school.");
+	        return;
+	    }
+
+	    for (Classroom c : classrooms) {
+	        System.out.println(c + "\n **************************");
+	    }
 	}
 
 	public void displayAllClubs() {
-		if (clubCount == 0) {
-			System.out.println("There are no clubs in the school.");
-			return;
-		}
 
-		for (int i = 0; i < clubCount; i++) {
-			System.out.println(clubs[i].toString() + "\n **************************");
-		}
+	    if (clubs.isEmpty()) {
+	        System.out.println("There are no clubs in the school.");
+	        return;
+	    }
+
+	    for (Club c : clubs) {
+	        System.out.println(c + "\n **************************");
+	    }
+	}
+	
+	public void displayAllPeople() {
+
+	    if (people.isEmpty()) {
+	        System.out.println("There are no clubs in the school.");
+	        return;
+	    }
+
+	    for (Person p : people) {
+	        System.out.println(p + "\n **************************");
+	    }
 	}
 
 	public double calculateMonthlyAmount() {
 		double total = 0;
 
-		for (int i = 0; i < peopleCount; i++) {
-			total += people[i].calculateMonthlyAmount();
+		for (Person p : people) {
+		    total += p.calculateMonthlyAmount();
 		}
-
+		
 		return total;
 	}
 
@@ -211,15 +209,16 @@ public class School implements Payable {
 	}
 
 	private int countStudentsRecursive(int index) {
-		if (index == peopleCount) {
-			return 0;
-		}
 
-		if (people[index] instanceof Student) {
-			return 1 + countStudentsRecursive(index + 1);
-		}
+	    if (index == people.size()) {
+	        return 0;
+	    }
 
-		return countStudentsRecursive(index + 1);
+	    if (people.get(index) instanceof Student) {
+	        return 1 + countStudentsRecursive(index + 1);
+	    }
+
+	    return countStudentsRecursive(index + 1);
 	}
 
     // Adds a student to a club and updates both the Student object and the Club object
@@ -307,56 +306,68 @@ public class School implements Payable {
 	}
 	
 	public double calculateAllSalaries() {
-		double allSalaries = 0;
-		for(int i = 0 ; i < peopleCount ; i++) {
-			if(people[i] instanceof Employee) {
-				Employee employee = (Employee) people[i];
-				allSalaries += employee.getBaseSalary() + employee.getAllowance();
-			}
-		}
-		return allSalaries;
+
+	    double allSalaries = 0;
+
+	    for (Person p : people) {
+
+	        if (p instanceof Employee) {
+
+	            Employee employee = (Employee) p;
+
+	            allSalaries += employee.getBaseSalary()
+	                         + employee.getAllowance();
+	        }
+	    }
+
+	    return allSalaries;
 	}
 	
 	public double calculateAllFees() {
-		double allFees = 0;
-		for(int i = 0 ; i < peopleCount ; i++) {
-			if(people[i] instanceof Student) {
-				allFees += people[i].calculateMonthlyAmount();
-			}
-			
-		}
-		return allFees;
+
+	    double allFees = 0;
+
+	    for (Person p : people) {
+
+	        if (p instanceof Student) {
+	            allFees += p.calculateMonthlyAmount();
+	        }
+	    }
+
+	    return allFees;
 	}
 	
 	
 	public String toString() {
-		return "School Name: " + name + "  People Count: " + peopleCount + "  Classroom Count: " + classroomCount+ "  Club Count: " + clubCount;
-	}
+		return "School Name: " + name +
+			       "  People Count: " + people.size() +
+			       "  Classroom Count: " + classrooms.size() +
+			       "  Club Count: " + clubs.size();	}
 	
 	public void saveLoginCount() {
 		PrintWriter writer = null;
 		try {
 			writer = new PrintWriter("login.txt");
-			for(int i = 0 ; i < peopleCount ; i++) {
+			for (Person p : people) {
 				
-				if(people[i] instanceof AdminStaff){
-					AdminStaff admin = (AdminStaff)people[i];
+				if(p instanceof AdminStaff){
+					AdminStaff admin = (AdminStaff)p;
 					writer.println(admin.getPosition() + " " + admin.getName() + " has logged in: " + admin.getLoginCount() + " times");
 				}
 				
-				else if(people[i] instanceof Teacher) {
-					Teacher teacher = (Teacher)people[i];
+				else if(p instanceof Teacher) {
+					Teacher teacher = (Teacher)p;
 					writer.println("Mr." + teacher.getName() + " has logged in: " + teacher.getLoginCount() + " times");
 				}
 				
-				else if(people[i] instanceof Employee) {
-					Employee e = (Employee)people[i];
+				else if(p instanceof Employee) {
+					Employee e = (Employee)p;
 					writer.println(e.getName() + " has logged in: " + e.getLoginCount() + " times");
 				}
 				
 				
-				else if(people[i] instanceof Student) {
-					Student student = (Student)people[i];
+				else if(p instanceof Student) {
+					Student student = (Student)p;
 					writer.println(student.getName() + " has logged in: " + student.getLoginCount() + " times");
 				}
 			}
